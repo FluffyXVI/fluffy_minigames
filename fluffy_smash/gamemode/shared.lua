c42cbe67f5b311c32aa9a50ac9e8d998c8674900
Robert Fraser <fluffyxvi@gmail.com>
2017-11-21 17:25:33 +1000

    Allow people to actually win in Smash

--- fluffy_smash/gamemode/shared.lua
+++ fluffy_smash/gamemode/shared.lua
@@ -5,6 +5,7 @@ GM.Author = 'FluffyXVI'
 
 GM.TeamBased = false	-- Is the gamemode FFA or Teams?
 GM.Elimination = true
+GM.WinBySurvival = true
 
 GM.RoundNumber = 10      -- How many rounds?
 GM.RoundTime = 90      -- Seconds each round lasts for
